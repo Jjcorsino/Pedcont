c6b80e93397c98dcda275922bbf8f23f110d09bf
jjcorsino <juliaan_44@outlook.com>
2022-06-02 17:31:34 -0300

    Parcial Algebra rec

--- scripts/recuperatorioalgebra.js
+++ scripts/recuperatorioalgebra.js
@@ -1,26 +1,25 @@
-const ctxa = 'donaAlgebra';
-const ctxa1 = 'barAlgebra';
-const ctxa2 = 'donaEstudiantesA';
-const ctxa3 = 'donaHombresA';
-const ctxa4 = 'donaMujeresA';
+const ctxa = 'donaRecuperatorioAlgebra';
+const ctxa1 = 'barRecuperatorioAlgebra';
+const ctxa2 = 'donaRecuperatorioEstudiantesA';
+const ctxa3 = 'donaRecuperatorioHombresA';
+const ctxa4 = 'donaRecuperatorioMujeresA';
 
 
 var pValues = ["Aprobados", "Desaprobados", "Promocionados", "Ausentes"];
-var xValues = ["Aprobados", "Desaprobados", "Promocionados"];
+var xValues = ["Aprobados", "Desaprobados"];
 var eValues = ["Hombres", "Mujeres"]; 
 
 
-const donaAlgebra = new Chart(ctxa, {
+const donaRecuperatorioAlgebra = new Chart(ctxa, {
     type: "pie",
     data: {
         labels: xValues,
         datasets: [{
             label: 'Total Notas Estudiantes',
-            data: [7, 68, 5],
+            data: [22, 67],
             backgroundColor: [
                 "#2b5797",
                 "#b91d47",
-                "#00aba9",
                 
             ],
             hoverOffset: 3
@@ -43,23 +42,21 @@ const donaAlgebra = new Chart(ctxa, {
     },
 });
 
-const barAlgebra = new Chart(ctxa1, {
+const barRecuperatorioAlgebra = new Chart(ctxa1, {
     type: 'bar',
     data: {
         labels: xValues,
         datasets: [{
-            data: [7, 68, 5],
+            data: [22, 67],
             label: " ",
             backgroundColor: [
                 'rgba(54, 162, 235, 0.5)',
                 'rgba(255, 99, 132, 0.5)',
-                'rgba(75, 192, 192, 0.5)',
             ],
 
             borderColor: [
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 99, 132, 1)',
-                'rgba(75, 192, 192, 1)',
             ],
 
             borderWidth: 1,
@@ -90,13 +87,13 @@ const barAlgebra = new Chart(ctxa1, {
 });
 
 
-const donaEstudiantesA = new Chart(ctxa2, {
+const donaRecuperatorioEstudiantesA = new Chart(ctxa2, {
     type: "doughnut",
     data: {
         labels: eValues,
         datasets: [{
             label: 'Estudiantes',
-            data: [68, 12],
+            data: [79, 10],
             backgroundColor: [
                 "rgba(54, 162, 235, 1)",
                 "rgba(255, 99, 132, 1)",
@@ -113,17 +110,16 @@ const donaEstudiantesA = new Chart(ctxa2, {
     }
 });
 
-const donaHombresA = new Chart(ctxa3, {
+const donaRecuperatorioHombresA = new Chart(ctxa3, {
     type: "pie",
     data: {
         labels: xValues,
         datasets: [{
             label: 'Estudiantes Hombres',
-            data: [5, 59, 4],
+            data: [18, 61],
             backgroundColor: [
                 "#2b5797",
                 "#b91d47",
-                "#00aba9",
                 
             ],
             hoverOffset: 3
@@ -142,17 +138,16 @@ const donaHombresA = new Chart(ctxa3, {
 });
 
 
-const donaMujeresA = new Chart(ctxa4, {
+const donaRecuperatorioMujeresA = new Chart(ctxa4, {
     type: "pie",
     data: {
         labels: xValues,
         datasets: [{
             label: 'Estudiantes Mujeres',
-            data: [2, 9, 1],
+            data: [4, 6],
             backgroundColor: [
                 "#2b5797",
                 "#b91d47",
-                "#00aba9",
                 
             ],
             hoverOffset: 3
